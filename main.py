a30d65b713746d687daaca5075a318aa4c6d2439
Kutluay <Yasar.Kutluay@ing.com>
2020-06-20 15:53:45 +0200

    included NLTK package sentiment analysis script

--- main.py
+++ main.py
@@ -1,29 +1,33 @@
+# Download
+# import nltk
+# nltk.download('vader_lexicon')
+# Inspiration: https://amiham-singh.github.io/ 
+# Small example: https://towardsdatascience.com/https-towardsdatascience-com-algorithmic-trading-using-sentiment-analysis-on-news-articles-83db77966704
 # Dependencies
 import os
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import spacy
-# Read file
 
-# Load English NLP decoder
-nlp = spacy.load("en_core_web_sm")
-
-# Load after text
+## Sentiment Analysis with NLTK
+# Initialize sentiment intensity analyzer (see https://www.nltk.org/api/nltk.sentiment.html)
+sia = SentimentIntensityAnalyzer()
+# Get before and after text files as single strings
 file_path = os.path.join(os.getcwd(), "data", "qaddafi", "after.txt")
 with open(file_path, 'r') as file:
-    text_full = file.read()
-# TODO: Count occurrences of \n in text_full to find number of paragraphs
-# Get rid of \n
-with open(file_path, 'r') as file:
-    text_after = file.read().replace("\n", "")
-# Tokenize
-token_after = nlp(text_after)
-
-# Load before text
+    after = file.read().replace("\n", "")
 file_path = os.path.join(os.getcwd(), "data", "qaddafi", "before.txt")
 with open(file_path, 'r') as file:
-    text_full = file.read()
-# TODO: Count occurrences of \n in text_full to find number of paragraphs
-# Get rid of \n
-with open(file_path, 'r') as file:
-    text_before = file.read().replace("\n", "")
+    before = file.read().replace("\n", "")
+# Get sentiment score dictionaries
+sentiments= {
+    "before" : sia.polarity_scores(before),
+    "after" : sia.polarity_scores(after)
+    }    
+
+## SpaCy Tokenization
+# Load English NLP decoder
+nlp = spacy.load("en_core_web_sm")
 # Tokenize
-token_before = nlp(text_before)
+token_before = nlp(before)
+token_after = nlp(after)
+# TODO: Count adjectives/verbs
