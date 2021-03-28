29ceb4883ab0a3ebabd37b81eed66b2c7b0f04ea
Kutluay <yasar.kutluay@ing.com>
2021-03-28 14:56:48 +0200

    made python main script run across all detected .txt files in leader folder

--- python_scripts/main.py
+++ python_scripts/main.py
@@ -10,6 +10,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 import spacy
 import pandas as pd
 import sys
+import re
 
 ## Sentiment Analysis with NLTK
 # Select political leader from command line
@@ -17,83 +18,72 @@ leader = sys.argv[1]
 # Initialize sentiment intensity analyzer (see https://www.nltk.org/api/nltk.sentiment.html)
 sia = SentimentIntensityAnalyzer()
 # Print
-print("Loading data")
-# Get rise and fall text files as single strings
-file_path = os.path.join(os.getcwd(), "data", leader, "fall.txt")
-with open(file_path, 'r', encoding = "utf-8") as file:
-    fall = file.read().replace("\n", "")
-file_path = os.path.join(os.getcwd(), "data", leader, "rise.txt")
-with open(file_path, 'r', encoding = "utf-8") as file:
-    rise = file.read().replace("\n", "")
-# Get sentiment score dictionaries
-sentiments = {
-    "rise" : sia.polarity_scores(rise),
-    "fall" : sia.polarity_scores(fall)
-    }    
+print("Loading and analyzing data")
+# Get list of all .txt files in relevant directory
+all_files = os.listdir(os.path.join(os.getcwd(), "data", leader))
+r = re.compile(".*.txt")
+all_files = list(filter(r.match, all_files))
+# Initialize sentiments dictonary
+sentiments = dict()
+# Estimate and store sentiments across all articles
+for f in all_files:
+    # Read file as single string
+    file_path = os.path.join(os.getcwd(), "data", leader, f)
+    with open(file_path, 'r', encoding = "utf-8") as file:
+        file_text = file.read().replace("\n", "")
+    # Estimate sentiment score and add to dictionary
+    sentiments[f.replace(".txt", "")] = sia.polarity_scores(file_text) 
 # Print
 print("The NLTK sentiment analysis is as follows:")
 for k, y in sentiments.items():
     print(k, "-->", y)
+
 ## SpaCy Tokenization
 # Print
 print("Initiating word type analysis")
 # Load English NLP decoder
 nlp = spacy.load("en_core_web_sm")
-# Tokenize
-token_before = nlp(rise)
-token_after = nlp(fall)
-## Count adjectives and verbs for both articles
-# For rise article
-# Initiate pandas dataframe
-pd_rise = pd.DataFrame({"word":[], "pos_value":[]})
-for token in token_before:
-    # Make row of data
-    sub_pd = pd.DataFrame({"word":str(token), "pos_value":token.pos_}, index = [0])
-    # Append to pd_rise
-    pd_rise = pd_rise.append(sub_pd, ignore_index = True)
-# Group by pos_value and count
-n_rows = len(pd_rise)
-pd_rise = (
-    pd_rise
-    .groupby("pos_value")
-    .agg(rise_count = ("pos_value", "count"))
-    .reset_index()
-    )
-# Get word count
-before_word_count = pd_rise[pd_rise["pos_value"] != "PUNCT"]["rise_count"].sum()
-# Get relative word type counts
-pd_rise["rise_relative_count"] = pd_rise["rise_count"]/n_rows
-
-# For fall article
-# Initiate pandas dataframe
-pd_fall = pd.DataFrame({"word":[], "pos_value":[]})
-for token in token_after:
-    # Make row of data
-    sub_pd = pd.DataFrame({"word":str(token), "pos_value":token.pos_}, index = [0])
-    # Append to pd_rise
-    pd_fall = pd_fall.append(sub_pd, ignore_index = True)
-# Group by pos_value and count
-n_rows = len(pd_fall)
-pd_fall = (
-    pd_fall
-    .groupby("pos_value")
-    .agg(fall_count = ("pos_value", "count"))
-    .reset_index()
-    )
-# Get word count
-after_word_count = pd_fall[pd_fall["pos_value"] != "PUNCT"]["fall_count"].sum()
-# Get relative word type counts
-pd_fall["fall_relative_count"] = pd_fall["fall_count"]/n_rows
-
-# Make into one general dataframe
-pd_both = (
-        pd_rise
-        .merge(pd_fall)
-        # Drop columns
-        .drop(["rise_count", "fall_count"], axis = 1)
-        # Make decimals easier to read
-        .round(2)
+# Initialize word count dictionary and final df
+word_count = dict()
+final_df = pd.DataFrame()
+# Get word analysis dataframe per file
+for f in all_files:
+    # Get article text and tokenize
+    file_path = os.path.join(os.getcwd(), "data", leader, f)
+    with open(file_path, 'r', encoding = "utf-8") as file:
+        file_text = file.read().replace("\n", "")
+    token = nlp(file_text)
+    # Initiate pandas dataframe
+    df = pd.DataFrame({"word":[], "pos_value":[]})
+    # Count adjectives/verbs
+    for t in token:
+        # Make row of data
+        sub_df = pd.DataFrame({"word":str(t), "pos_value":t.pos_}, index = [0])
+        # Append to df
+        df = df.append(sub_df, ignore_index = True)
+        # Group by pos_value and count
+    n_rows = len(df)
+    df = (
+        df
+        .groupby("pos_value")
+        .agg(count = ("pos_value", "count"))
+        .reset_index()
         )
+    # Store word count
+    word_count[f.replace(".txt", "")] = df[df["pos_value"] != "PUNCT"]["count"].sum()
+    # Get relative word type counts
+    df[f.replace(".txt", "") + "_relative_count"] = df["count"]/n_rows
+    df.drop("count", axis = 1, inplace = True)
+    # Add to final df
+    if len(final_df) == 0:
+        final_df = df
+    else:
+        final_df = (
+            final_df
+            .merge(df)
+            .round(2)
+            )
+
 # Print
-print("The overall share of word types in both articles:")
-print(pd_both.to_string(index = False))
+print("The overall share of word types in all articles:")
+print(final_df.to_string(index = False))
